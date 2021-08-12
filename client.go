79dce7d536cefa076ea3e897c01eb8d22f4cf85b
thiago.nascimento <tiware23@gmail.com>
2021-08-12 18:11:46 -0300

    Update client.go

--- client.go
+++ client.go
@@ -1,66 +1,27 @@
-// Script to get and post Pod object from k8s api manager
 package main
 
 import (
-	"bytes"
 	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
+	"kubernetes/client"
 	"net/http"
-	"os"
 )
-
-func checkError(err error) {
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func httpMethod(method, uri, token string, body io.Reader) *http.Response {
-	client := &http.Client{}
-	req, err := http.NewRequest(method, uri, body)
-	checkError(err)
-
-	bearer := "Bearer " + token
-	req.Header.Add("Authorization", bearer)
-
-	resp, err := client.Do(req)
-	checkError(err)
-
-	return resp
-}
-
-func getPod(uri string, token string) {
-	res := httpMethod("GET", uri, token, nil)
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	checkError(err)
-	var prettyJSON bytes.Buffer
-	_ = json.Indent(&prettyJSON, body, "", "\t")
-	log.Println(string(prettyJSON.Bytes()))
-}
-
-func postPod(uri string, token string) {
-	body, err := os.Open("pod.json")
-	res := httpMethod("POST", uri, token, body)
-	checkError(err)
-	defer body.Close()
-
-	fmt.Println("Response status: ", res.Status)
-	io.Copy(os.Stdout, res.Body)
-
-}
 
 func main() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	urlApi := "https://kubernetes.docker.internal:6443/api/v1/namespaces/default/pods"
+	urlApi := "url"
 	token := "<token>"
 
-	getPod(urlApi, token)
-	// postPod(urlApi, token)
+	postRequest := client.PostPod(urlApi, token)
+	if postRequest == 201 {
+		fmt.Printf("Created Pod status: %d\n", postRequest)
+	} else if postRequest == 409 {
+		fmt.Printf("The resource already exists status: %d\n", postRequest)
+	} else {
+		fmt.Println("Found error durind the process")
+	}
+
+	request := client.GetPod(urlApi, token)
+	fmt.Println(request)
 
 }
